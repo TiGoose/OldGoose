e9098ae60b324d8a224a0ed9c4fb22689bdc35cf
angela~ <53804450+angelawangits@users.noreply.github.com>
2023-04-22 17:41:47 +0800

    clear bundle ticket page

--- lib/mont_saint_michel_traffic.dart
+++ lib/mont_saint_michel_traffic.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 class TrafficWidget extends StatefulWidget {
   @override
   _TrafficWidgetState createState() => _TrafficWidgetState();
 }
 
 class _TrafficWidgetState extends State<TrafficWidget> {
+  String _inputText = '';
+
+  void _updateInputText(String text) {
+    setState(() {
+      _inputText = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // Figma Flutter Generator TrafficWidget - FRAME
@@ -15,347 +22,80 @@ class _TrafficWidgetState extends State<TrafficWidget> {
         width: 402,
         height: 870,
         decoration: const BoxDecoration(
-          color : Color.fromRGBO(255, 255, 255, 1),
+          color: Color.fromRGBO(255, 255, 255, 1),
         ),
-        child: Stack(
-            children: <Widget>[
-              Positioned(
-                  top: 0,
-                  left: 0,
-                  child: Container(
-                      width: 402,
-                      height: 60,
-                      decoration: const BoxDecoration(
-                        color : Color.fromRGBO(171, 182, 194, 1),
-                      )
-                  )
-              ),const Positioned(
-                  top: 16,
-                  left: 14,
-                  child: Text('Traffic', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 24,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1
-                  ),)
-              ),const Positioned(
-                  top: 72,
-                  left: 25,
-                  child: Text('From', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),Positioned(
-                  top: 101,
-                  left: 25,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius : const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                        bottomLeft: Radius.circular(5),
-                        bottomRight: Radius.circular(5),
-                      ),
-                      color : const Color.fromRGBO(255, 255, 255, 1),
-                      border : Border.all(
-                        color: const Color.fromRGBO(214, 214, 214, 1),
-                        width: 1,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
+        child: Column(children: <Widget>[
+          Positioned(
+              top: 0,
+              left: 0,
+              child: Container(
+                width: 402,
+                height: 60,
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(171, 182, 194, 1),
+                ),
+                child: Text('聖米歇爾山套票~~~~',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        fontFamily: 'Inter',
+                        fontSize: 24,
+                        letterSpacing:
+                            0 /*percentages not used in flutter. defaulting to zero*/,
+                        fontWeight: FontWeight.normal,
+                        height: 1)),
+              )),
+          const Positioned(
+              top: 72,
+              left: 25,
+              child: Text(
+                '請選擇您的組合',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(0, 0, 0, 1),
+                    fontFamily: 'Inter',
+                    fontSize: 14,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1.5),
+              )),
+          Positioned(
+            top: 100,
+            left: 25,
+            child: ElevatedButton(
+              onPressed: () {
+                // 按下第一個按鈕後的處理邏輯
+              },
+              child: const Text('組合 a : +上午票'),
+            ),
+          ),
+          Positioned(
+            top: 150,
+            left: 25,
+            child: ElevatedButton(
+              onPressed: () {
+                // 按下第一個按鈕後的處理邏輯
+              },
+              child: const Text('組合 b : +上午票'),
+            ),
+          ),
 
-                      children: <Widget>[
-                        const Text('Paris', textAlign: TextAlign.left, style: TextStyle(
-                            color: Color.fromRGBO(44, 55, 110, 1),
-                            fontFamily: 'Arial',
-                            fontSize: 14,
-                            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1
-                        ),), const SizedBox(width : 1),
-                        Container(
-                            width: 1,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color : Color.fromRGBO(44, 55, 110, 1),
-                            )
-                        ),
-
-                      ],
-                    ),
-                  )
-              ),const Positioned(
-                  top: 152,
-                  left: 27,
-                  child: Text('To', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),const Positioned(
-                  top: 232,
-                  left: 28,
-                  child: Text('Date', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),Positioned(
-                  top: 181,
-                  left: 27,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius : const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                        bottomLeft: Radius.circular(5),
-                        bottomRight: Radius.circular(5),
-                      ),
-                      color : const Color.fromRGBO(255, 255, 255, 1),
-                      border : Border.all(
-                        color: const Color.fromRGBO(214, 214, 214, 1),
-                        width: 1,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-
-                      children: <Widget>[
-                        const Text('Nice', textAlign: TextAlign.left, style: TextStyle(
-                            color: Color.fromRGBO(44, 55, 110, 1),
-                            fontFamily: 'Arial',
-                            fontSize: 14,
-                            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1
-                        ),), const SizedBox(width : 1),
-                        Container(
-                            width: 1,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color : Color.fromRGBO(44, 55, 110, 1),
-                            )
-                        ),
-
-                      ],
-                    ),
-                  )
-              ),const Positioned(
-                  top: 394,
-                  left: 28,
-                  child: Text('Adult', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),Positioned(
-                  top: 423,
-                  left: 28,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius : const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                        bottomLeft: Radius.circular(5),
-                        bottomRight: Radius.circular(5),
-                      ),
-                      color : const Color.fromRGBO(255, 255, 255, 1),
-                      border : Border.all(
-                        color: const Color.fromRGBO(214, 214, 214, 1),
-                        width: 1,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-
-                      children: <Widget>[
-                        const Text('1', textAlign: TextAlign.left, style: TextStyle(
-                            color: Color.fromRGBO(44, 55, 110, 1),
-                            fontFamily: 'Arial',
-                            fontSize: 14,
-                            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1
-                        ),), const SizedBox(width : 1),
-                        Container(
-                            width: 1,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color : Color.fromRGBO(44, 55, 110, 1),
-                            )
-                        ),
-
-                      ],
-                    ),
-                  )
-              ),const Positioned(
-                  top: 474,
-                  left: 28,
-                  child: Text('Child', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),Positioned(
-                  top: 503,
-                  left: 28,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius : const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                        bottomLeft: Radius.circular(5),
-                        bottomRight: Radius.circular(5),
-                      ),
-                      color : const Color.fromRGBO(255, 255, 255, 1),
-                      border : Border.all(
-                        color: const Color.fromRGBO(214, 214, 214, 1),
-                        width: 1,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-
-                      children: <Widget>[
-                        const Text('0', textAlign: TextAlign.left, style: TextStyle(
-                            color: Color.fromRGBO(44, 55, 110, 1),
-                            fontFamily: 'Arial',
-                            fontSize: 14,
-                            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1
-                        ),), const SizedBox(width : 1),
-                        Container(
-                            width: 1,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color : Color.fromRGBO(44, 55, 110, 1),
-                            )
-                        ),
-
-                      ],
-                    ),
-                  )
-              ),const Positioned(
-                  top: 557,
-                  left: 28,
-                  child: Text('Email', textAlign: TextAlign.left, style: TextStyle(
-                      color: Color.fromRGBO(0, 0, 0, 1),
-                      fontFamily: 'Inter',
-                      fontSize: 14,
-                      letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                      fontWeight: FontWeight.normal,
-                      height: 1.5
-                  ),)
-              ),Positioned(
-                  top: 586,
-                  left: 28,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius : const BorderRadius.only(
-                        topLeft: Radius.circular(5),
-                        topRight: Radius.circular(5),
-                        bottomLeft: Radius.circular(5),
-                        bottomRight: Radius.circular(5),
-                      ),
-                      color : const Color.fromRGBO(255, 255, 255, 1),
-                      border : Border.all(
-                        color: const Color.fromRGBO(214, 214, 214, 1),
-                        width: 1,
-                      ),
-                    ),
-                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-
-                      children: <Widget>[
-                        const Text('og@mail.com', textAlign: TextAlign.left, style: TextStyle(
-                            color: Color.fromRGBO(44, 55, 110, 1),
-                            fontFamily: 'Arial',
-                            fontSize: 14,
-                            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                            fontWeight: FontWeight.normal,
-                            height: 1
-                        ),), const SizedBox(width : 1),
-                        Container(
-                            width: 1,
-                            height: 14,
-                            decoration: const BoxDecoration(
-                              color : Color.fromRGBO(44, 55, 110, 1),
-                            )
-                        ),
-
-                      ],
-                    ),
-                  )
-              ),Positioned(
-                  top: 653,
-                  left: 29,
-                  child: Container(
-                      width: 334,
-                      height: 57,
-                      decoration: const BoxDecoration(
-
-                      ),
-                      child: Stack(
-                          children: <Widget>[
-                            Positioned(
-                                top: 0,
-                                left: 0,
-                                child: Container(
-                                    width: 334,
-                                    height: 57,
-                                    decoration: const BoxDecoration(
-                                      borderRadius : BorderRadius.only(
-                                        topLeft: Radius.circular(5),
-                                        topRight: Radius.circular(5),
-                                        bottomLeft: Radius.circular(5),
-                                        bottomRight: Radius.circular(5),
-                                      ),
-                                      color : Color.fromRGBO(5, 10, 48, 1),
-                                    )
-                                )
-                            ),const Positioned(
-                                top: 15,
-                                left: 26,
-                                child: Text('立即訂購', textAlign: TextAlign.center, style: TextStyle(
-                                    color: Color.fromRGBO(255, 255, 255, 1),
-                                    fontFamily: 'Inter',
-                                    fontSize: 24,
-                                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                                    fontWeight: FontWeight.normal,
-                                    height: 1
-                                ),)
-                            ),
-                          ]
-                      )
-                  )
-              ),
-            ]
-        )
-    );
+          const Positioned(
+              top: 500,
+              left: 26,
+              child: Text(
+                '立即訂購',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromRGBO(20, 20, 10, 0),
+                    fontFamily: 'Inter',
+                    fontSize: 24,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+        ]));
   }
 }
